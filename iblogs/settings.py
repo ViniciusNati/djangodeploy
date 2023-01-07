7148b8c7f505cf64d191a7831acad6ae7c5461e3
ViniciusNati <59131289+Krauller@users.noreply.github.com>
2023-01-07 17:08:23 -0300

    go

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dr=aa^8!duo!%bl@t2l(hcx*2!rx)50qrp@dwpoqb#)wqh@9$x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -114,14 +114,11 @@ STATICFILES_FINDERS = (
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR/'static']
 
-
-#########################################
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
