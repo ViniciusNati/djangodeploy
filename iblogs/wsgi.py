59276093671a3609cc9756331ced6b4d5ebb776c
ViniciusNati <59131289+Krauller@users.noreply.github.com>
2023-01-07 03:52:55 -0300

    sad
    asd

--- iblogs/wsgi.py
+++ iblogs/wsgi.py
@@ -11,6 +11,6 @@ import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iblogs.settings")
-django.setup()
+
 
 application = get_wsgi_application()
