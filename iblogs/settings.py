6158cc653704deb1744b2aa0f40a74febb375225
ViniciusNati <59131289+Krauller@users.noreply.github.com>
2023-01-07 01:21:27 -0300

    asd
    asd

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -107,12 +107,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'web-production-d0bc.up.railway.app/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, '/static/')
+    os.path.join(BASE_DIR, 'static')
 ]
-#####media
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
