1a669f7816d94e832e54e613059ff4629b6d5551
ViniciusNati <59131289+Krauller@users.noreply.github.com>
2023-01-07 02:03:57 -0300

    aa
    aa

--- iblogs/wsgi.py
+++ iblogs/wsgi.py
@@ -6,15 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
-
 import os
-import sys
-
-path = 'web-production-d0bc.up.railway.app'  # insira o caminho para o seu projeto aqui
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'iblogs.settings'
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iblogs.settings')
+
 application = get_wsgi_application()
