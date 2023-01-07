dfc247222683f3ec5465e2485222e34ae4570a9c
ViniciusNati <59131289+Krauller@users.noreply.github.com>
2023-01-07 03:55:04 -0300

    asd
    asd

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -115,7 +115,7 @@ STATICFILES_FINDERS = (
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
