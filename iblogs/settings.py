16a2c0c535efa78d3a702045d04294b3a67dfc02
ViniciusNati <59131289+Krauller@users.noreply.github.com>
2023-01-07 17:18:31 -0300

    aaa

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     "django.middleware.security.SecurityMiddleware",
+"whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -114,11 +116,13 @@ STATICFILES_FINDERS = (
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATICFILES_DIRS = [BASE_DIR/'static']
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
--- iblogs/wsgi.py
+++ iblogs/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 import os
-import django
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iblogs.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iblogs.settings")
+from whitenoise import WhiteNoise
+
 
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root="static")
+application.add_files("/staticfiles/", prefix="static")
