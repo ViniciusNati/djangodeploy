ca9ea78379beb93170375731aa1848d8013f7ec7
ViniciusNati <59131289+Krauller@users.noreply.github.com>
2023-01-07 00:18:27 -0300

    novo

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
      'material',
     'material.admin',
-   # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
